79253f4a1927d9d9ceadd16b45cb50c195dc2a83
cherrydan <cherrydan.1978@gmail.com>
2022-09-28 22:25:34 +0300

    Refactor making connection code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,34 +10,45 @@ public class Main {
     private static final String DB_PASS = "jw8s0F4";
 
     public static void main(String[] args) {
-        try {
-            Class.forName(DB_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-            e.printStackTrace();
-            return;
-        }
 
-        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        loadDriver();
         Connection connection;
-
-        try {
-            connection = DriverManager
-                    .getConnection(DB_URL, DB_USER, DB_PASS);
-
-        } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            e.printStackTrace();
-            return;
-        }
+        connection = getConnection();
 
         if (connection != null) {
             System.out.println("You successfully connected to database now");
+
         } else {
             System.out.println("Failed to make connection to database");
         }
 
 
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName(Main.DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+
+        }
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+    }
+
+    private static Connection getConnection() {
+        Connection connection;
+
+        try {
+            connection = DriverManager
+                    .getConnection(Main.DB_URL, Main.DB_USER, Main.DB_PASS);
+            return connection;
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
